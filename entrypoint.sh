2060397efc9a813e3b5c35e5134ca78fbf61d4ba
Daku3011 <rdwarkesh1300@gmail.com>
2026-06-22 17:49:32 +0530

    chore(auth): set default superuser credentials to suanalytics/admin in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,12 +18,10 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 import os
 User = get_user_model()
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'suanalytics')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@su-analytics.in')
-if not password:
-    print('WARNING: DJANGO_SUPERUSER_PASSWORD not set. Skipping superuser creation.')
-elif not User.objects.filter(username=username).exists():
+if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
     print(f'Superuser created: {username}')
 else:
